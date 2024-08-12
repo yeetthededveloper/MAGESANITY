df2fd8e4813646575df4b3a047329363a1c3db58
yeetthededveloper <yeetmanku@gmail.com>
2024-08-12 13:24:28 -0700

    setup

--- src/main/java/com/example/examplemod/Magesanity.java
+++ src/main/java/com/example/examplemod/Magesanity.java
@@ -30,8 +30,8 @@ import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(ExampleMod.MODID)
-public class ExampleMod
+@Mod(Magesanity.MODID)
+public class Magesanity
 {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "examplemod";
@@ -61,7 +61,7 @@ public class ExampleMod
                 output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
             }).build());
 
-    public ExampleMod()
+    public Magesanity()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 

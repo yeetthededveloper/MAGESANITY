2815b580009f9953e9380c61d315e5f9a0f6c310
YeEtMaNKu <110639069+YeEtMaNKu@users.noreply.github.com>
2024-08-26 11:10:05 -0700

    Update withcaulblock.java

--- src/main/java/net/mryeetman/magesanity/block/costom/withcaulblock.java
+++ src/main/java/net/mryeetman/magesanity/block/costom/withcaulblock.java
@@ -2,11 +2,14 @@ package net.mryeetman.magesanity.block.costom;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.Nullable;
 
 public class withcaulblock extends BaseEntityBlock {
+    public static final VoxelShape SHAPE = Block.box(0, 0, 0, 16, 12 ,16);
     protected withcaulblock(Properties pProperties) {
         super(pProperties);
     }

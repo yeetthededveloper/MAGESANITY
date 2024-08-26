15b06accf6681cbfab9c07520157a2cb2415645c
YeEtMaNKu <110639069+YeEtMaNKu@users.noreply.github.com>
2024-08-26 11:12:50 -0700

    Update withcaulblock.java

--- src/main/java/net/mryeetman/magesanity/block/costom/withcaulblock.java
+++ src/main/java/net/mryeetman/magesanity/block/costom/withcaulblock.java
@@ -1,17 +1,32 @@
 package net.mryeetman.magesanity.block.costom;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.Nullable;
 
 public class withcaulblock extends BaseEntityBlock {
     public static final VoxelShape SHAPE = Block.box(0, 0, 0, 16, 12 ,16);
+
+
     protected withcaulblock(Properties pProperties) {
         super(pProperties);
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
+        return super.getShape(pState, pLevel, pPos, pContext);
+    }
+
+    @Override
+    public RenderShape getRenderShape(BlockState pState) {
+        return RenderShape.MODEL;
     }
 
     @Nullable

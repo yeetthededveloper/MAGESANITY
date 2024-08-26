9b43f91cd2a334ca30bfd359b73c89ad08688991
YeEtMaNKu <110639069+YeEtMaNKu@users.noreply.github.com>
2024-08-26 11:37:54 -0700

    Update ModBlockTagGenerator.java

--- src/main/java/net/mryeetman/magesanity/datagen/ModBlockTagGenerator.java
+++ src/main/java/net/mryeetman/magesanity/datagen/ModBlockTagGenerator.java
@@ -4,13 +4,14 @@ import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraftforge.common.data.BlockTagsProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import net.mryeetman.magesanity.Magesanity;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.concurrent.CompletableFuture;
 
 public class ModBlockTagGenerator extends BlockTagsProvider {
-    public ModBlockTagGenerator(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider, String modId, @Nullable ExistingFileHelper existingFileHelper) {
-        super(output, lookupProvider, modId, existingFileHelper);
+    public ModBlockTagGenerator(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider,  @Nullable ExistingFileHelper existingFileHelper) {
+        super(output, lookupProvider, Magesanity.MOD_ID, existingFileHelper);
     }
 
     @Override

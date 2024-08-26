e77813de233cb4eb1f7694b8b58e6c911d5f3c35
YeEtMaNKu <110639069+YeEtMaNKu@users.noreply.github.com>
2024-08-26 11:02:42 -0700

    Update withcaulblock.java

--- src/main/java/net/mryeetman/magesanity/block/costom/withcaulblock.java
+++ src/main/java/net/mryeetman/magesanity/block/costom/withcaulblock.java
@@ -1,4 +1,19 @@
 package net.mryeetman.magesanity.block.costom;
 
-public class withcaulblock {
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.Nullable;
+
+public class withcaulblock extends BaseEntityBlock {
+    protected withcaulblock(Properties pProperties) {
+        super(pProperties);
+    }
+
+    @Nullable
+    @Override
+    public BlockEntity newBlockEntity(BlockPos blockPos, BlockState blockState) {
+        return null;
+    }
 }

9437bf7632aefba4969edc25528c2ec868f9c489
YeEtMaNKu <110639069+YeEtMaNKu@users.noreply.github.com>
2024-08-26 11:49:10 -0700

    Update ModItemModelProvider.java

--- src/main/java/net/mryeetman/magesanity/datagen/ModItemModelProvider.java
+++ src/main/java/net/mryeetman/magesanity/datagen/ModItemModelProvider.java
@@ -1,4 +1,28 @@
 package net.mryeetman.magesanity.datagen;
 
-public class ModItemModelProvider {
+import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.client.model.generators.ItemModelBuilder;
+import net.minecraftforge.client.model.generators.ItemModelProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.RegistryObject;
+import net.mryeetman.magesanity.Magesanity;
+import net.mryeetman.magesanity.item.ModItems;
+
+public class ModItemModelProvider extends ItemModelProvider {
+    public ModItemModelProvider(PackOutput output,  ExistingFileHelper existingFileHelper) {
+        super(output, Magesanity.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void registerModels() {
+        simpleItem(ModItems.EXITEM);
+
+    }
+    private ItemModelBuilder simpleItem(RegistryObject<Item> item) {
+        return withExistingParent(item.getId().getPath(),
+                new ResourceLocation("item/generated")).texture("layer0",
+                new ResourceLocation(Magesanity.MOD_ID,"item/" + item.getId().getPath()));
+    }
 }

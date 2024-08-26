a258912e6d1cd2395ccfc4adfca8f45d5431e964
YeEtMaNKu <110639069+YeEtMaNKu@users.noreply.github.com>
2024-08-26 11:36:42 -0700

    Update ModBlockStateProvider.java

--- src/main/java/net/mryeetman/magesanity/datagen/ModBlockStateProvider.java
+++ src/main/java/net/mryeetman/magesanity/datagen/ModBlockStateProvider.java
@@ -1,4 +1,26 @@
 package net.mryeetman.magesanity.datagen;
 
-public class ModBlockStateProvider {
+import net.minecraft.data.PackOutput;
+import net.minecraftforge.client.model.generators.BlockStateProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.RegistryObject;
+import net.mryeetman.magesanity.Magesanity;
+import net.minecraft.world.level.block.Block;
+import net.mryeetman.magesanity.block.ModBlocks;
+
+public class ModBlockStateProvider extends BlockStateProvider {
+    public ModBlockStateProvider(PackOutput output, ExistingFileHelper exFileHelper) {
+        super(output, Magesanity.MOD_ID, exFileHelper);
+    }
+
+    @Override
+    protected void registerStatesAndModels() {
+        blockWithItem(ModBlocks.JOMAMA_BLOCK);
+
+    }
+
+    private void blockWithItem(RegistryObject<Block> blockRegistryObject) {
+        simpleBlockWithItem(blockRegistryObject.get(), cubeAll(blockRegistryObject.get()));
+    }
+
 }
